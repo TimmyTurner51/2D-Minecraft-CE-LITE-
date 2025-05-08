89f0a50b2fb70cf79093bffb36e06144ea69052f
TimmyCraft <46533729+TimmyTurner51@users.noreply.github.com>
2025-05-07 22:29:25 -0500

    almost borked this git lol. Fixed! ;)

---
+++ src/gfx/Arm_2.h
@@ -0,0 +1,18 @@
+#ifndef Arm_2_include_file
+#define Arm_2_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define Arm_2_width 13
+#define Arm_2_height 5
+#define Arm_2_size 67
+#define Arm_2 ((gfx_sprite_t*)Arm_2_data)
+extern unsigned char Arm_2_data[67];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/gfx/Body_4.h
@@ -0,0 +1,18 @@
+#ifndef Body_4_include_file
+#define Body_4_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define Body_4_width 5
+#define Body_4_height 13
+#define Body_4_size 67
+#define Body_4 ((gfx_sprite_t*)Body_4_data)
+extern unsigned char Body_4_data[67];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
